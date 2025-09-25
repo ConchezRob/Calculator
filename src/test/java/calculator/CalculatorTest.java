daca279736d18011c3a6d441bea5fb0aed67bfd7
Conchez-Boueytou Robin <r.conchez-boueytou@etud.univ-pau.fr>
2025-09-25 10:16:01 +0200

    version 1

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,10 +2,40 @@ package calculator;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
 
 class CalculatorTest {
 
     @Test
-    void
+    void add_devrait_retouner_somme_des_deux_int()
+    {
+        //GIVEN
+        Calculator calculator = new Calculator();
+        int opG = 5;
+        int opD = 3;
+        int expected = 8;
+
+        //WHEN
+        int result =calculator.add(opG,opD);
+
+        //THEN
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void divide_devrait_retouner_quotient_des_deux_int()
+    {
+        //GIVEN
+        Calculator calculator = new Calculator();
+        int opG = 6;
+        int opD = 3;
+        int expected = 2;
+
+        //WHEN
+        int result =calculator.divide(opG,opD);
+
+        //THEN
+        assertThat(result).isEqualTo(expected);
+    }
 }
